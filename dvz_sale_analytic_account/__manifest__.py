4517051d24bcb6e3e1340b45312f34b4062e227b
imran <muhammad.imran@evantage-soft.com>
2026-08-16 22:49:07 +0500

    [UPDATE] UPDATED

---
+++ dvz_sale_analytic_account/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "DVZ Sale Order Analytic Account",
+    "version": "19.0.1.0.0",
+    "summary": "Restores a single header-level Analytic Account field on Sale Orders/Quotations (like Odoo 16), propagating it to each order line's analytic distribution",
+    "author": "Genius Valley",
+    "category": "Sales",
+    "license": "OPL-1",
+    "depends": [
+        "sale",
+        "analytic",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
