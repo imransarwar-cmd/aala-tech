8a27353d72945901119a7883035e87a517d38c25
imran <muhammad.imran@evantage-soft.com>
2026-08-17 22:31:00 +0500

    [UPDATE] uPDATED

--- dvz_sale_analytic_account/__manifest__.py
+++ dvz_sale_analytic_account/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "DVZ Sale Order Analytic Account",
+    "name": "Sale Order Analytic Account",
     "version": "19.0.1.0.0",
     "summary": "Restores a single header-level Analytic Account field on Sale Orders/Quotations (like Odoo 16), propagating it to each order line's analytic distribution",
     "author": "Genius Valley.",
--- dvz_sale_analytic_account/models/sale_order.py
+++ dvz_sale_analytic_account/models/sale_order.py
@@ -16,56 +16,56 @@ class SaleOrder(models.Model):
              "distribution to that account across every order line that "
              "doesn't already have its own distribution set.",
     )
-
-    def _dvz_apply_analytic_account_to_lines(self, lines=None):
-        """Push this order's analytic_account_id onto the given lines'
-        analytic_distribution (100% to that single account). Only touches
-        lines that don't already have a distribution, so it never silently
-        overwrites analytics someone has deliberately set per line."""
-        for order in self:
-            if not order.analytic_account_id:
-                continue
-            target_lines = lines if lines is not None else order.order_line
-            target_lines = target_lines.filtered(
-                lambda l: l.order_id == order and not l.analytic_distribution and not l.display_type
-            )
-            if target_lines:
-                distribution = {str(order.analytic_account_id.id): 100}
-                target_lines.write({"analytic_distribution": distribution})
-
-    @api.onchange("analytic_account_id")
-    def _onchange_dvz_analytic_account_id(self):
-        for order in self:
-            if order.analytic_account_id:
-                distribution = {str(order.analytic_account_id.id): 100}
-                for line in order.order_line.filtered(lambda l: not l.display_type):
-                    line.analytic_distribution = distribution
-
-    def write(self, vals):
-        res = super().write(vals)
-        if "analytic_account_id" in vals:
-            self._dvz_apply_analytic_account_to_lines()
-        return res
-
-    def action_confirm(self):
-        res = super().action_confirm()
-        self._dvz_apply_analytic_account_to_lines()
-        return res
-
-
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        lines = super().create(vals_list)
-        for line in lines:
-            if (
-                line.order_id.analytic_account_id
-                and not line.analytic_distribution
-                and not line.display_type
-            ):
-                line.analytic_distribution = {
-                    str(line.order_id.analytic_account_id.id): 100
-                }
-        return lines
+#
+#     def _dvz_apply_analytic_account_to_lines(self, lines=None):
+#         """Push this order's analytic_account_id onto the given lines'
+#         analytic_distribution (100% to that single account). Only touches
+#         lines that don't already have a distribution, so it never silently
+#         overwrites analytics someone has deliberately set per line."""
+#         for order in self:
+#             if not order.analytic_account_id:
+#                 continue
+#             target_lines = lines if lines is not None else order.order_line
+#             target_lines = target_lines.filtered(
+#                 lambda l: l.order_id == order and not l.analytic_distribution and not l.display_type
+#             )
+#             if target_lines:
+#                 distribution = {str(order.analytic_account_id.id): 100}
+#                 target_lines.write({"analytic_distribution": distribution})
+#
+#     @api.onchange("analytic_account_id")
+#     def _onchange_dvz_analytic_account_id(self):
+#         for order in self:
+#             if order.analytic_account_id:
+#                 distribution = {str(order.analytic_account_id.id): 100}
+#                 for line in order.order_line.filtered(lambda l: not l.display_type):
+#                     line.analytic_distribution = distribution
+#
+#     def write(self, vals):
+#         res = super().write(vals)
+#         if "analytic_account_id" in vals:
+#             self._dvz_apply_analytic_account_to_lines()
+#         return res
+#
+#     def action_confirm(self):
+#         res = super().action_confirm()
+#         self._dvz_apply_analytic_account_to_lines()
+#         return res
+#
+#
+# class SaleOrderLine(models.Model):
+#     _inherit = "sale.order.line"
+#
+#     @api.model_create_multi
+#     def create(self, vals_list):
+#         lines = super().create(vals_list)
+#         for line in lines:
+#             if (
+#                 line.order_id.analytic_account_id
+#                 and not line.analytic_distribution
+#                 and not line.display_type
+#             ):
+#                 line.analytic_distribution = {
+#                     str(line.order_id.analytic_account_id.id): 100
+#                 }
+#         return lines
